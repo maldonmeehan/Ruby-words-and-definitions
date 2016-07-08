5c14024bb687d6992d4f298a2cf814a869345277
maldon <maldonmeehan@gmail.com>
2016-07-08 09:29:41 -0700

    add passing test for letting the user enter two definitions

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,15 +1,16 @@
 class Definition
+  @@definitions = []
 
-  define_method(:initialize) do |description1|
+  define_method(:initialize) do |description1, description2|
     @description1 = description1
-    # @description2 = description2
+    @description2 = description2
   end
 
   define_method(:description1) do
     @description1
   end
 
-#   define_method(:description2) do
-#     @description2
-#   end
+  define_method(:description2) do
+    @description2
+  end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -7,14 +7,14 @@ end
 
   describe("#description1") do
     it("lets the user add a definition") do
-      test_definition = Definition.new("A Hash is a dictionary-like collection of unique keys and their values.")
+      test_definition = Definition.new("A Hash is a dictionary-like collection of unique keys and their values.", "Also called associative arrays, they are similar to Arrays, but where an Array uses integers as its index, a Hash allows you to use any object type.")
       expect(test_definition.description1()).to(eq("A Hash is a dictionary-like collection of unique keys and their values."))
     end
   end
 
-  # describe("#definition") do
-  #   it("lets the user add a second definition") do
-  #     test_definition = Definition.new("Also called associative arrays, they are similar to Arrays, but where an Array uses integers as its index, a Hash allows you to use any object type.")
-  #     expect(test_definition.description2()).to(eq("Also called associative arrays, they are similar to Arrays, but where an Array uses integers as its index, a Hash allows you to use any object type."))
-  #   end
-  # end
+  describe("#description2") do
+    it("lets the user add a second definition") do
+      test_definition = Definition.new("A Hash is a dictionary-like collection of unique keys and their values.", "Also called associative arrays, they are similar to Arrays, but where an Array uses integers as its index, a Hash allows you to use any object type.")
+      expect(test_definition.description2()).to(eq("Also called associative arrays, they are similar to Arrays, but where an Array uses integers as its index, a Hash allows you to use any object type."))
+    end
+  end
