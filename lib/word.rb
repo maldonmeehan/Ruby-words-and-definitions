ac1455d4a267b3114639eef1f28fa33afd6833d9
maldon <maldonmeehan@gmail.com>
2016-07-08 08:28:19 -0700

    add passing test for letting the user add a word

---
+++ lib/word.rb
@@ -0,0 +1,10 @@
+class Word
+
+  define_method(:initialize) do |list|
+    @list = list
+  end
+
+  define_method(:list) do
+    @list
+  end
+end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -6,8 +6,8 @@ describe('word') do
 end
 
 describe("#word") do
-    it("returns a word to the user") do
+    it("lets the user add a word") do
       test_word = Word.new("Hash")
-      expect(test_word.make()).to(eq("Hash"))
+      expect(test_word.list()).to(eq("Hash"))
     end
   end
