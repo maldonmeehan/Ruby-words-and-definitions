938d72f2c317ba326145ac9d73324d96504b625e
maldon <maldonmeehan@gmail.com>
2016-07-08 14:07:28 -0700

    delet views and start over

--- app.rb
+++ app.rb
@@ -2,20 +2,3 @@ require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("./lib/word")
-
-get('/') do
-  @words = Word.all()
-  erb(:index)
-end
-
-post("/confirmation") do
-  list = params.fetch("word")
-  word = Word.new(list)
-  word.save()
-  erb(:confirmation)
-end
-
-get('/definition') do
-  @definitions = Definition.all()
-  erb(:definition)
-end
