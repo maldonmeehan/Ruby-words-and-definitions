6f5123b8c6b78816c8cb57e1304d636697a2e4a5
maldon <maldonmeehan@gmail.com>
2016-07-08 09:13:34 -0700

    add a failing test for letting the user add a second definition

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -11,3 +11,10 @@ end
       expect(test_definition.description()).to(eq("A Hash is a dictionary-like collection of unique keys and their values."))
     end
   end
+
+  describe("#definition2") do
+    it("lets the user add a second definition") do
+      test_definition = Definition.new("Also called associative arrays, they are similar to Arrays, but where an Array uses integers as its index, a Hash allows you to use any object type.")
+      expect(test_definition.description2()).to(eq("Also called associative arrays, they are similar to Arrays, but where an Array uses integers as its index, a Hash allows you to use any object type."))
+    end
+  end
