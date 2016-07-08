e0fcf126198a04e6cb46c16ec42e7bf330851080
maldon <maldonmeehan@gmail.com>
2016-07-08 11:06:59 -0700

    add confirmation page

--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("./lib/word")
-require("/lib/definition")
 
 get('/') do
+  @words = Word.all()
   erb(:index)
+end
+
+post("/confirmation") do
+  list = params.fetch("word")
+  word = Word.new(list)
+  word.save()
+  erb(:confirmation)
 end
