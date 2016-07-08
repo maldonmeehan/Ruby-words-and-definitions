0f742686f5835b0c09b1caac580deb5c1abfa905
maldon <maldonmeehan@gmail.com>
2016-07-08 09:36:32 -0700

    add passing test for definition empty at first and change the name of the definition array

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,5 +1,5 @@
 class Definition
-  @@definitions = []
+  @@all_definitions = []
 
   define_method(:initialize) do |description1, description2|
     @description1 = description1
@@ -12,5 +12,9 @@ class Definition
 
   define_method(:description2) do
     @description2
+  end
+
+  define_singleton_method(:all) do
+    @@all_definitions
   end
 end
