6e7a2319661559ed1d1220e1138c48a1adf79432
maldon <maldonmeehan@gmail.com>
2016-07-08 14:31:40 -0700

    add content to index and new word form

--- app.rb
+++ app.rb
@@ -2,3 +2,12 @@ require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("./lib/word")
+
+get("/") do
+  @words = Word.all()
+  erb(:index)
+end
+
+get('new_word_form') do
+  erb(:new_word_form)
+end
