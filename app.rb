d30fcc6191c67043bd5b65e87c88ac220cd1c099
Maldon <maldonmeehan@gmail.com>
2016-07-15 08:16:01 -0700

    add failing test for the add a defintion route

--- app.rb
+++ app.rb
@@ -19,3 +19,22 @@ post('/words') do
   @word.save()
   erb(:word_success)
 end
+
+get('/words/:id') do
+  @word = Word.find(params.fetch('id').to_i())
+  erb(:word)
+end
+
+get('/words/:id/definitions/new') do
+  @word = Word.find(params.fetch('id').to_i())
+  erb(:definition_form)
+end
+
+post('/definitions') do
+  @definition_main = params.fetch('definition_main')
+  @definition = Definition.new({:definition_main => @definition_main})
+  @definition.save()
+  @word = Word.find(params.fetch('word_id').to_i())
+  @word.add_definition(@definition)
+  erb(:definition_success)
+end
--- spec/word_definition_intergration_spec.rb
+++ spec/word_definition_intergration_spec.rb
@@ -3,12 +3,28 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe("the word route", {:type => :feature}) do
+describe("the add a word route", {:type => :feature}) do
   it("takes the user to a page where they can enter a word") do
+    visit('/')
+    click_link("Add A Word")
+  end
+end
+
+describe("the create a word route", {:type => :feature}) do
+  it("takes the user to a page where they can see their entered word") do
     visit('/')
     click_link("Add A Word")
     fill_in("name", :with => "Hash")
     click_button("Add")
     expect(page).to have_content("Hash")
+  end
+end
+
+describe("the defintion route", {:type => :feature}) do
+  it("takes the user to a page where they can enter a deifintion") do
+    visit('/')
+    click_link("Hash")
+    # expect(page).to have_content("Hash")
+    # click_link("Add A Definition")
   end
 end
