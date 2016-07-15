f0309665a94f22eb5713b040f262bab11415268c
Maldon <maldonmeehan@gmail.com>
2016-07-14 21:26:47 -0700

    add initial view file stucture and content to index page

--- app.rb
+++ app.rb
@@ -8,25 +8,3 @@ get('/') do
   @words = Word.all()
   erb(:index)
 end
-
-post("/confirmation") do
-  list = params.fetch("word")
-  word = Word.new(list)
-  word.save()
-  erb(:confirmation)
-end
-
-post('/') do
-  erb(:index)
-end
-
-get('/new_word_form') do
-  erb(:new_word_form)
-end
-
-post('/new_word_form') do
-  list = params.fetch("word")
-  word = Word.new(list)
-  word.save()
-  erb(:new_word_form)
-end
