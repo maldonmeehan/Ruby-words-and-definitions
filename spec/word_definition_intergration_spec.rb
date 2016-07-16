552e7ab3059d29e79e680fe80b1039fa8cf95952
Maldon <maldonmeehan@gmail.com>
2016-07-16 10:20:37 -0700

    fix spelling errors and add to the add a defintion route intergration spec

--- spec/word_definition_intergration_spec.rb
+++ spec/word_definition_intergration_spec.rb
@@ -1,4 +1,5 @@
 require('capybara/rspec')
+require('launchy')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
@@ -28,8 +29,10 @@ end
 describe("the add a defintion route", {:type => :feature}) do
   it("takes the user to a page where they can enter a deifintion") do
     visit('/')
-    click_link("Hash")
-    # expect(page).to have_content("Hash")
-    # click_link("Add A Definition")
+    click_link("Add A Word")
+    fill_in("name", :with => "Object")
+    click_button("Add")
+    click_link("Add a definition")
+    expect(page).to have_content("Add a definition")
   end
 end
