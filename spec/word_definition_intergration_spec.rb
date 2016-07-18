2d2482a40841933ba04ab4371371b9b41e6ba984
Maldon <maldonmeehan@gmail.com>
2016-07-18 05:51:45 -0700

    add passing spec for defintion route

--- spec/word_definition_intergration_spec.rb
+++ spec/word_definition_intergration_spec.rb
@@ -20,13 +20,13 @@ describe('the add word route', {:type => :feature}) do
 end
 
 describe('the add definition route', {:type => :feature}) do
-  it('takes the user to a page where they can enter a deifintion') do
+  it('clicks on the add a definition link and displays the definition that has been added') do
     visit('/')
     click_link('Hash')
     expect(page).to have_content('Hash')
     click_link('Add a Definition')
     expect(page).to have_content('Add a definition')
-    fill_in('definition', :with => 'A Hash is a dictionary-like collection of unique keys and their values.')
+    fill_in('definition-one', :with => 'A Hash is a dictionary-like collection of unique keys and their values.')
     click_button('Add Definition')
     expect(page).to have_content('A Hash is a dictionary-like collection of unique keys and their values.')
   end
